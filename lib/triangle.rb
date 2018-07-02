bc21902cf5c453585b2f00e328536b59cb70dfbe
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-02 19:02:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,11 @@ class Triangle
   end 
   
   def type  
-      if @side1 == @side2 == @side3 
+      if @side1 == @side2 && @side1 == @side3 
         puts :equilateral 
       elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
         puts :isosceles 
-      elsif @side1 != @side2 != @side3 
+      elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3 
         puts :scalene
       end 
   end 
