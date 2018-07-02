c6ee3db64eb986ccd6de7835e8a631c066640c4f
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-02 18:53:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,20 @@
 class Triangle
   
+  attr_accessor :side1, :side2, :side3
+  # attr_reader :equilateral, :isosceles, :scalene
+  
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+  end 
+  
+  def equilateral 
+      
+  end 
+  
+  def valid?
+    
   end 
   
   def kind 
